d67046fd276424dbc3e2e55c3db739782c962faa
NST-solucao <74821264+NST-solucao@users.noreply.github.com>
2022-03-04 12:12:58 -0300

    Update questao_02.py

--- questao_02.py
+++ questao_02.py
@@ -1,9 +1,9 @@
 n = [1,5,3,4,2]
 x = 2
 iv = sorted(n, reverse=True)
-t = len(n)
+
 count = 0
-for i in range(t):
+for i in range(len(n)):
     for element in iv:
         if iv[i] - element == x:
             # print(f'{iv[i]} - {element}') # Para testar retorno da subtração entre a lista[elementos]
